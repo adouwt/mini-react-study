572cea4889a55c248a9e540fa0ebf24eabb114ee
adou-pc <1259709654@qq.com>
2023-04-08 13:52:39 +0800

    first commiet

---
+++ src/index.fiber.jsx
@@ -0,0 +1,251 @@
+function createDom(fiber) {
+    const dom = fiber.type === 'TEXT_ELEMENT' ? document.createTextNode("") : document.createElement(fiber.type)
+  
+    updateDom(dom, {}, fiber.props)
+  
+  
+    return dom
+  }
+  
+  let nextUnitOfWork = null
+  let wipRoot = null // 保存着对root fiber的引用
+  let currentRoot = null // 保存着当前页面对应的fiber tree
+  let deletions = null
+  function render(element, container){
+    wipRoot = {
+      dom: container,
+      props: {
+        children: [element], // 此时的element还只是React.createElement函数创建的virtual dom树
+      },
+      alternate: currentRoot,
+    }
+    deletions = []
+    nextUnitOfWork = wipRoot
+  }
+  function commitRoot(){
+    deletions.forEach(commitWork)
+    commitWork(wipRoot.child)
+    currentRoot = wipRoot
+    wipRoot = null
+  }
+  
+  const isEvent = key => key.startsWith("on")
+  const isProperty = key => key !== "children" && !isEvent(key)
+  const isNew = (prev, next) => key => prev[key] !== next[key]
+  const isGone = (prev, next) => key => !(key in next)
+  function updateDom(dom, prevProps, nextProps) {
+    //Remove old or changed event listeners
+    Object.keys(prevProps)
+      .filter(isEvent)
+      .filter(
+        key =>
+          !(key in nextProps) ||
+          isNew(prevProps, nextProps)(key)
+      )
+      .forEach(name => {
+        const eventType = name
+          .toLowerCase()
+          .substring(2)
+        dom.removeEventListener(
+          eventType,
+          prevProps[name]
+        )
+      })
+  
+    // Remove old properties
+    Object.keys(prevProps)
+      .filter(isProperty)
+      .filter(isGone(prevProps, nextProps))
+      .forEach(name => {
+        dom[name] = ""
+      })
+  
+    // Set new or changed properties
+    Object.keys(nextProps)
+      .filter(isProperty)
+      .filter(isNew(prevProps, nextProps))
+      .forEach(name => {
+        dom[name] = nextProps[name]
+      })
+  
+    // Add event listeners
+    Object.keys(nextProps)
+      .filter(isEvent)
+      .filter(isNew(prevProps, nextProps))
+      .forEach(name => {
+        const eventType = name
+          .toLowerCase()
+          .substring(2)
+        dom.addEventListener(
+          eventType,
+          nextProps[name]
+        )
+      })
+  }
+  function commitWork(fiber){
+    if(!fiber){
+      return
+    }
+    const domParent = fiber.parent.dom;
+    if (fiber.effectTag === "PLACEMENT" && fiber.dom != null) {
+      domParent.appendChild(fiber.dom)
+    } else if (fiber.effectTag === "UPDATE" && fiber.dom != null) {
+      updateDom(
+        fiber.dom,
+        fiber.alternate.props,
+        fiber.props
+      )
+    } else if (fiber.effectTag === "DELETION") {
+      domParent.removeChild(fiber.dom)
+    }
+    commitWork(fiber.child)
+    commitWork(fiber.sibling)
+  }
+  
+  function workLoop(deadline) {
+    let shouldYield = false
+    while (nextUnitOfWork && !shouldYield) {
+      nextUnitOfWork = performUnitOfWork(nextUnitOfWork)
+      shouldYield = deadline.timeRemaining() < 1
+    }
+    if(!nextUnitOfWork && wipRoot){
+      commitRoot()
+    }
+    requestIdleCallback(workLoop)
+  }
+  
+  requestIdleCallback(workLoop)
+  function reconcileChildren(wipFiber, elements) {
+    let index = 0
+    let oldFiber =
+        wipFiber.alternate && wipFiber.alternate.child
+    let prevSibling = null
+  
+    while (index < elements.length || oldFiber != null) {
+      const element = elements[index]
+      let newFiber = null
+  
+      const sameType = oldFiber && element && element.type == oldFiber.type
+  
+      if (sameType) {
+        newFiber = {
+          type: oldFiber.type,
+          props: element.props,
+          dom: oldFiber.dom,
+          parent: wipFiber,
+          alternate: oldFiber,
+          effectTag: "UPDATE",
+        }
+      }
+      if (element && !sameType) {
+        newFiber = {
+          type: element.type,
+          props: element.props,
+          dom: null,
+          parent: wipFiber,
+          alternate: null,
+          effectTag: "PLACEMENT",
+        }
+      }
+      if (oldFiber && !sameType) {
+        oldFiber.effectTag = "DELETION"
+        deletions.push(oldFiber)
+      }
+  
+      if (oldFiber) {
+        oldFiber = oldFiber.sibling
+      }
+  
+      if (index === 0) {
+        wipFiber.child = newFiber
+      } else if (element) {
+        prevSibling.sibling = newFiber
+      }
+  
+      prevSibling = newFiber
+      index++
+    }
+  }
+  function performUnitOfWork(fiber) {
+    // 第一步 根据fiber节点创建真实的dom节点，并保存在fiber.dom属性中
+    if(!fiber.dom){
+      fiber.dom = createDom(fiber)
+    }
+  
+    // 第二步 将当前fiber节点的真实dom添加到父节点中，注意，这一步是会触发浏览器回流重绘的！！！
+    // if(fiber.parent){
+    //   fiber.parent.dom.appendChild(fiber.dom)
+    // }
+    // 第三步 给子元素创建对应的fiber节点
+    const children = fiber.props.children
+    // let prevSibling
+    // children.forEach((child, index) => {
+    //   const newFiber = {
+    //     type: child.type,
+    //     props: child.props,
+    //     parent: fiber,
+    //     dom: null
+    //   }
+    //   if(index === 0){
+    //     fiber.child = newFiber
+    //   } else {
+    //     prevSibling.sibling = newFiber
+    //   }
+    //   prevSibling = newFiber
+    // })
+    reconcileChildren(fiber, children)
+  
+    // 第四步，查找下一个工作单元
+    if(fiber.child){
+      return fiber.child
+    }
+    let nextFiber = fiber
+    while(nextFiber){
+      if(nextFiber.sibling){
+        return nextFiber.sibling
+      }
+      nextFiber = nextFiber.parent
+    }
+   
+  }
+  const MiniReact = {
+    createElement:  (type, props, ...children) => {
+      return {
+        type,
+        props: {
+          ...props,
+          children: children.map(child => {
+            if(typeof child === 'object'){
+              return child
+            }
+            return {
+              type: 'TEXT_ELEMENT',
+              props: {
+                nodeValue: child,
+                children: [],
+              }
+            }
+          })
+        }
+      }
+    },
+    render
+  }
+  /** @jsx MiniReact.createElement */
+  const container = document.getElementById("root")
+  
+  const updateValue = e => {
+    rerender(e.target.value)
+  }
+  
+  const rerender = value => {
+    const element = (
+      <div>
+        <input onInput={updateValue} value={value} />
+        <h2>Hello {value}</h2>
+      </div>
+    )
+    MiniReact.render(element, container)
+  }
+  
+  rerender("World")
---
+++ webpack.configfiber.js
@@ -0,0 +1,20 @@
+module.exports = {
+    entry : './src/index.fiber.jsx',
+    output: {
+        filename: 'bundlefiber.js'
+    },
+    module: { // 所有第三方模块的匹配规则， webpack默认只能处理.js后缀名的文件，需要安装第三方loader
+        rules: [
+            {
+                test: /\.jsx/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                },
+                exclude: /(node_modules)/, // 千万别忘记添加exclude选项,不然运行可能会报错
+            }, 
+        ]
+    }
+}
